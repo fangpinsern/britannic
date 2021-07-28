bf43ea7e17c442c31142782ba6144da7f00e3167
pinsern <pinsern@PS-Mac.com>
2021-07-28 11:44:17 +0800

    Added venue model

---
+++ src/models/venue.model.js
@@ -0,0 +1,32 @@
+const { Schema, connection } = require("mongoose");
+const bcrypt = require("bcrypt");
+
+const UserSchema = new Schema(
+  {
+    name: { type: String, required: true, unique: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    isAdmin: { type: Boolean, required: true, default: false },
+    isApproved: { type: Boolean, required: true, default: false },
+  },
+  {
+    collection: "user",
+    timestamps: true,
+  }
+);
+
+UserSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) {
+    return next();
+  }
+
+  try {
+    const hash = await bcrypt.hash(this.password, 12);
+    this.password = hash;
+    return next();
+  } catch (err) {
+    return next(err);
+  }
+});
+
+module.exports = connection.model("UserSchema", UserSchema);
