f14b1af933fa24a35b554fb6cdfffeb8b9af944c
pinsern <pinsern@PS-Mac.com>
2021-07-29 16:14:00 +0800

    Code clean up

--- src/controllers/venue/createChildVenue.controller.js
+++ src/controllers/venue/createChildVenue.controller.js
@@ -63,18 +63,3 @@ const createChildVenueController = async (req, res, next) => {
 };
 
 module.exports = { createChildVenueController };
-
-// name: { type: String, required: true },
-//     capacity: { type: Number, required: true },
-//     openingHours: { type: String, required: true },
-//     priorityEmails: { type: [String] },
-//     description: { type: String, maxLength: 500 },
-//     visible: { type: Boolean, default: true, required: true },
-//     image: { type: String, required: false },
-//     parentVenue: { type: Types.ObjectId, ref: "VenueSchema" },
-//     isChildVenue: { type: Boolean, default: false, required: true },
-//     childVenue: {
-//       type: [{ type: Types.ObjectId, ref: "VenueSchema" }],
-//       required: false,
-//       default: [],
-//     },
