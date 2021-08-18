9f6d03b9fe2e0a29bed834908ec7d8a07655d23d
pinsern <pinsern@PS-Mac.com>
2021-08-18 11:33:31 +0800

    Updated venue model to accomodate instant booking, updateVenueInstantBookController

---
+++ src/controllers/venue/updateVenueInstantBook.controller.js
@@ -0,0 +1,33 @@
+const { BAD_REQUEST, ACCEPTED } = require("http-status");
+const Venue = require("../../models/venue.model");
+
+const updateVenueInstantBookController = async (req, res, next) => {
+  const venueId = req.params.venueId;
+
+  let venueFound;
+  try {
+    venueFound = await Venue.findOne({ _id: venueId });
+  } catch (err) {
+    return next(err);
+  }
+
+  if (!venueFound) {
+    const message = "No venue found by given Id";
+    const err = errorFormatter(message, BAD_REQUEST);
+    return next(err);
+  }
+
+  const updateVenueInstantBook = !venueFound.isInstantBook;
+  venueFound.isInstantBook = updateVenueInstantBook;
+
+  let venueSaved;
+  try {
+    venueSaved = await venueFound.save();
+  } catch (err) {
+    return next(err);
+  }
+
+  return res.status(ACCEPTED).json({ venue: venueSaved.toObject() });
+};
+
+module.exports = { updateVenueInstantBookController };
